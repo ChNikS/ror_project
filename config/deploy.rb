0c1a726df4957687b98767fe7fb3ef465bce4fd2
ChNikS <cherednik.nickita@yandex.ru>
2016-11-13 22:48:17 -0800

    deploy2

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'https://github.com/ChNikS/ror_project.git'
 set :deploy_to, '/home/deployer/ror_project'
 set :deploy_user, 'deployer'
 
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'private_pub.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'private_pub.yml', '.env'
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
