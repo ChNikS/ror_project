345536633a8b22cc12cca841e7574ae39b89d4a2
ChNikS <cherednik.nickita@yandex.ru>
2016-11-18 03:35:19 -0800

    deploy private_pub fix2

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,20 +7,9 @@ set :repo_url, 'https://github.com/ChNikS/ror_project.git'
 set :deploy_to, '/home/deployer/ror_project'
 set :deploy_user, 'deployer'
 
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'private_pub.yml', 'config/private_pub_thin.yml','.env'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/private_pub.yml', 'config/private_pub_thin.yml','.env'
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
-
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-end
 
 namespace :private_pub do
   desc 'Start private_pub server'
@@ -33,7 +22,7 @@ namespace :private_pub do
       end
     end
   end
-  
+
   desc 'Stop private_pub server'
   task :stop do
     on roles(:app) do
@@ -44,7 +33,7 @@ namespace :private_pub do
       end
     end
   end
-  
+
   desc 'Restart private_pub server'
   task :restart do
     on roles(:app) do
